0b03dd6872828b0404f349154996149549736a2f
abelgacem <amarbelgacem@hotmail.fr>
2024-10-10 10:24:50 +0000

    upd

--- assets/js/table.js
+++ assets/js/table.js
@@ -1,3 +1,9 @@
+// $(document).ready(function() {
+//   $('#myTable').DataTable();
+// });
+
 $(document).ready(function() {
-  $('#myTable').DataTable();
+  $('.sortable').each(function() {
+    $(this).DataTable();
+  });
 });
